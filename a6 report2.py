ebe939f820c22fda5c11a670d7cac33d4c1a63a1
nwrigh04 <46507178+nwrigh04@users.noreply.github.com>
2019-03-12 19:54:32 -0400

    Update a6 report2.py

--- a6 report2.py
+++ a6 report2.py
@@ -3,17 +3,21 @@ DB_FILE = 'payroll_dc_small.db'
 conn = sqlite3.connect(DB_FILE)
 
 def main():
-    print('a6 - standard report 1')
+    print('a6 - standard report 2')
     strsql = """select 
-                    l.loct location, 
-                    max(ft.salary) sal_max, 
-                    min(ft.salary) sal_min, 
-                    count(ft.salary) people_count
-                from factdata_mar2016 ft, loc l
-                where ft.loc = l.loc
-                group by location
-                order by sal_max desc
+                    supervist, 
+                    max(salary), 
+                    avg(salary), 
+                    min(salary), 
+                    max(los), 
+                    avg(los) los_avg, 
+                    count(supervist)
+                from factdata_mar2016, super
+                where factdata_mar2016.supervis = super.supervis
+                group by supervist
+                order by los_avg asc;
                 limit 20;
+            
         """
     cursor = conn.execute(strsql)
     # the formating string for each row of the report
@@ -21,15 +25,15 @@ def main():
     # printing the header of the report. 
     print(
         report_string_format.format(
-            'location', 'sal_max', 'sal_min', 'people_count'
+            'supervist', 'sal_max', 'avg_sal', 'sal_min', 'max_los', 'los_avg' 'count_supervist'
             )
         )
 
     for row in cursor:  
-        location, sal_max, sal_min, people_count = row
+        supervist, sal_max, avg_sal, sal_min, max_los, los_avg, count_supervist = row
         print(
             report_string_format.format(
-                location, sal_max, sal_min, people_count
+                supervist, sal_max, avg_sal, sal_min, max_los, los_avg, count_supervist
                 )
             )
 
